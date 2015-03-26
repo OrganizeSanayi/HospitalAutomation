7121e0675ba69b69edf241d4a764e9ad282c7d69
Sait Sami Kocatas <sait.kocatas@cmak.com>
2015-03-26 14:49:54 +0200

    [4] Create logger & validation utility tools

---
+++ HospitalAutomation/Util/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalAutomation.Util
+{
+    class Logger
+    {
+        /**
+         *  Basic logging tool, it creates a file named logs under bin directory
+         * 
+         *  Usage:
+         * Logger logger = new Logger();
+         * logger.log("test");
+         */
+        public void log (String logMessage){
+            System.IO.StreamWriter file = new System.IO.StreamWriter("../logs.txt", true);
+            file.WriteLine(logMessage);
+
+            file.Close();
+        }
+    }
+}
---
+++ HospitalAutomation/Util/ValidationUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalAutomation
+{
+    class ValidationUtil
+    {
+        /*
+         * Returns false if the passed text is empty
+         */
+        public bool isEmptyString(String text)
+        {
+            text = text.Trim();
+            return text == null || text == String.Empty ? false : true;
+        }
+    }
+}
