a49ce031e39300754bc8014dc1c73840471064b0
rabiaokumus <rabiaokumus@gmail.com>
2015-03-29 17:08:43 +0300

    Copy master

---
+++ HospitalAutomation/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HospitalAutomation
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            //Deneme
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //txtUserName ve txtUserPassword ile sisteme giriş yapılacak
+        }
+    }
+}
