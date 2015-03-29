8c04948bed0d404305cb56e3fbe515da076b5b9d
Deliganli <saitkocatas@gmail.com>
2015-03-29 17:24:11 +0300

    [Repair] Revert logger & Validator

---
+++ HospitalAutomation/Util/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalAutomation.Util
+{
+    class Logger
+    {
+        /**
+         *  Basic logging tool, it creates a file named logs under bin directory
+         * 
+         *  Usage:
+         * Logger.i("test");
+         */
+        public static void i(String logMessage)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter("../logs.txt", true);
+            file.WriteLine(logMessage);
+
+            file.Close();
+        }
+    }
+}
