0c2432d76cd38022df5ee25fcf6ffdd9cfc152c0
Deliganli <saitkocatas@gmail.com>
2015-05-30 22:07:21 +0300

    Fix store procedure warning

--- HospitalAutomation.Model/HospitalEntity.Context.cs
+++ HospitalAutomation.Model/HospitalEntity.Context.cs
@@ -39,11 +39,11 @@ namespace HospitalAutomation.Model
         public virtual DbSet<TETKIKRAPORRESIMLER> TETKIKRAPORRESIMLERs { get; set; }
         public virtual DbSet<TETKIKRAPORYOL> TETKIKRAPORYOLs { get; set; }
         public virtual DbSet<BOLUMLER> BOLUMLER { get; set; }
-        public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<STATU> STATU { get; set; }
         public virtual DbSet<ADLISAGLIKKURULU> ADLISAGLIKKURULU { get; set; }
         public virtual DbSet<MUAYENEEPIKRIZ> MUAYENEEPIKRIZ { get; set; }
         public virtual DbSet<TETKIKRAPORLAR> TETKIKRAPORLAR { get; set; }
+        public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
